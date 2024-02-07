15103074782720e1d31148b758593f1f240cfddc
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-07 17:33:27 -0500

    some css to the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import {useNavigate} from 'react-router-dom';
 import {FcGoogle} from 'react-icons/fc';
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
-// import './Login.css';
+import './Login.css';
 import { GoogleLogin } from "@react-oauth/google";
 import { client} from '../client';
 
@@ -31,15 +31,12 @@ export default function Login(){
             })
     }
  
-    return(
-        // <div className='login-container'>
-        //    <div className='video-container'>
+    return(  
         <GoogleOAuthProvider clientId={process.env.SHAREME_GOOGLE_API}>
-            <div className="flex justify-start items-center flex-col h-screen">
-                <div className="relative w-full h-full">
+            <div className='login-container'>
+            <div className='video-container'>
                 <video
-                    // className="video-element"
-                    className="w-full h-full object-cover"
+                    className="video-element"
                     src={shareVideo}
                     typeof='video/mp4'
                     loop
@@ -47,8 +44,8 @@ export default function Login(){
                     muted
                     autoPlay
                 />
-                <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
-                    <div className="p-5">
+                <div className="login-container">
+                    <div className="image-logo">
                         <img src={logo} alt="logoImg" width="130px"/>
                     </div>
 
