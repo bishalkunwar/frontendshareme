6f409c17f9672b2f432db7cb7dcbf0fc4de47529
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-07 20:45:21 -0500

    simple folder structure setups

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,8 +1,18 @@
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import {Link, Route, Routes} from 'react-router-dom';
+
+import Sidebar from "../components/Sidebar";
+import UserProfile from "../components/UserProfile";
+import Pins from './Pins'
+import {client} from '../client';
+import logo from '../assets/logo.png';
+
 export default function Home(){
 
     return(
         <div>
-            Home
+            <Sidebar/>
         </div>
     )
 };
---
+++ src/container/Pins.js
@@ -0,0 +1,9 @@
+ 
+ export default function Pins(){
+
+    return(
+        <div>
+            pins
+        </div>
+    )
+ }
