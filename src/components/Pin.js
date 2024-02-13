ee91f3b9b9805ec4378bf310816cf21cdb88746c
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-12 19:20:31 -0500

    pin or the post

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,7 +1,24 @@
 
+import { useState } from "react";
+import {Link, useNavigate} from 'react-router-dom';
+import {v4 as uuidv4} from 'uuid';
+import {MdDownloadForOffline} from 'react-icons/md';
+import {AiTwotoneDelete} from 'react-icons/ai';
+import {BsFillArrowUpRightCircleFill} from 'react-icons/bs';
 
-export const Pin = () => {
+import {client, urlFor} from '../client';
 
+export const Pin = ({pin}) => {
+
+    const [postHovered, setPostHovered] = useState(false);
+    const[savingPost, setSavingPost] = useState(false);
+    const navigate = useNavigate();
+
+    const {postedBy, image, _id, destination} = pin;
+    const user = localStorage.getItem('user')!=='undefined'?JSON.parse(localStorage('user')):localStorage.clear();
+    
+    let alreadySaved = pin?.save?.filter((item)=>item.postedBy?._id === user?.googleId);
+    
     return(
         <div>
             pin
