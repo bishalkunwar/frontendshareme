5256cb8e1c893e2e645c9b225f1f35e4fe7b6420
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-14 21:40:27 -0500

    create pin imports

--- src/components/CreatePin.js
+++ src/components/CreatePin.js
@@ -1,4 +1,13 @@
 
+import {AiOutlineCloudUpload} from "react-icons/ai";
+import {useNavigate} from "react-router-dom"
+import {MdDelete} from "react-icons/md";
+
+import {categories} from "../utils/data";
+import {client} from "../client";
+import Spinner from "./Spinner";
+
+
 const CreatePin = () => {
 
     return(
