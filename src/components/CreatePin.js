2015f804089bc81b360f4aaf6e8896d1a25c9893
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-20 20:00:18 -0500

    image file uplad input field with condition

--- src/components/CreatePin.js
+++ src/components/CreatePin.js
@@ -9,7 +9,7 @@ import Spinner from "./Spinner";
 import { useState } from "react";
 
 
-const CreatePin = () => {
+const CreatePin = ({user}) => {
 
     const[title, setTitle] = useState('');
     const [about, setabout] = useState('');
@@ -20,9 +20,59 @@ const CreatePin = () => {
     const [imageAsset, setimageAsset] = useState('');
     const [wrongImageType, setwrongImageType] = useState(false);
 
+    const navigate = useNavigate();
+
+    const uploadImage = (e) => {
+        const selectedFile  = e.target.files[0];
+        if(selectedFile.type === 'image/png' || selectedFile.type === "image/svg" || selectedFile.type === "jpef" || selectedFile.type === "gif" || selectedFile.type === "image/tigg"){
+
+        }else{
+            setloading(true);
+            setwrongImageType(true);
+        }
+    };
+
     return(
-        <div>
-            create pin.
+        <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
+            {fields && (
+                <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in">
+                    Please Add all fields.
+                </p>
+            )}
+            <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full">
+                <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+                    <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+                        {loading && (
+                            <Spinner/>
+                        )}{
+                            wrongImageType && (
+                                <p>Wrong file type, sorry.</p>
+                            )
+                        }
+                        {!imageAsset ? (
+                            <label>
+                                <div className="flex flex-col items-center justify-center h-full">
+                                    <div className="flex flex-col justify-center items-center">
+                                        <p><AiOutlineCloudUpload/></p>
+                                        <p className="text-lg">Click to Upload</p>
+                                    </div>
+                                    <p className="mt-32 text-gray-400">
+                                        Recommendation: Use High-Quality JPG, JPEG, SVG, PNG, GIF or TIFF Less than 20MB.
+                                    </p>
+                                </div>
+                                <input type="file" name="upload-image" onChange={uploadImage} className="w-0 h-0"/>
+                            </label>
+                        ):(
+                            <div>
+
+                            </div>
+                        );
+                        
+                        }
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 };
