35078780a953396e1c910aede222e2810f20fd99
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-20 19:35:36 -0500

    creating the fieldValues to create pin at createQuery

--- src/components/CreatePin.js
+++ src/components/CreatePin.js
@@ -6,9 +6,19 @@ import {MdDelete} from "react-icons/md";
 import {categories} from "../utils/data";
 import {client} from "../client";
 import Spinner from "./Spinner";
+import { useState } from "react";
 
 
 const CreatePin = () => {
+
+    const[title, setTitle] = useState('');
+    const [about, setabout] = useState('');
+    const [loading, setloading] = useState(false);
+    const [destination, setdestination] = useState('');
+    const [fields, setfields] = useState('');
+    const [category, setcategory] = useState('');
+    const [imageAsset, setimageAsset] = useState('');
+    const [wrongImageType, setwrongImageType] = useState(false);
 
     return(
         <div>
