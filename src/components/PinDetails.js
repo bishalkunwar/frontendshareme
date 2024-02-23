229eae8501be2f4715f5afeb95448fb44fa212ca
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-22 23:03:20 -0500

    download icon to the bottom area of image

--- src/components/PinDetails.js
+++ src/components/PinDetails.js
@@ -22,7 +22,15 @@ const PinDetails = ({user}) => {
             <div className="flex justify-center items-center md:items-start flex-initial">
                 <img src={(pinDetail?.image && urlFor(pinDetail?.image).url())} alt="user-post" className="rounded-t-3xl rounded-b-lg"/>
             </div>
-            <div>
+            <div className="w-full p-5 flex-1 xl:min-w-620">
+                <div className="flex items-center justify-between">
+                    <div className="flex gap-2 items-center">
+                        <a href={`${pinDetail.image.assset.url}?dl=`} download className="bg-secondaryColor p-2 text-xl rounded-full flex items-center justify-center text-dark opacity-75 hover:opacity-100">
+                            <MdDownloadForOffline/>
+                        </a>
+                    </div>
+                    
+                </div>
                 
             </div>
         </div>
