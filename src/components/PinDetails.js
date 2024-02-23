df27f1b9abb417a4eb4a64c3e6d35eb8eb93971a
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-22 21:29:09 -0500

    rendering pinDetail image and defining variable state at pinDetail comp

--- src/components/PinDetails.js
+++ src/components/PinDetails.js
@@ -1,12 +1,32 @@
 
+import {useState, useEffect} from "react";
+import {MdDownloadForOffline} from "react-icons/md";
+import {Link, useParams} from "react-router-dom";
+import {v4 as uuidv4} from "uuid";
 
-const PinDetails = () => {
+import {client, urlFor} from "../client";
+import MasonaryLayout from "./MasonaryLayout";
+import {pinDetailsMorePinQuery} from "../utils/data";
+import Spinner from "./Spinner";
+
+const PinDetails = ({user}) => {
+
+    const {pinId} = useParams();
+    const[pins, setPins] = useState();
+    const[pinDetail, setPinDetail] = useState();
+    const[comment, setComment] = useState('')
+    const[addingComment, setAddingComment] = useState(false);
 
     return (
-        <div>
-            pin details.
+        <div className="flex xl:flex-row flex-col m-auto bg-white" style={{maxWidth: '1500px', borderRadius: '32px'}}>
+            <div className="flex justify-center items-center md:items-start flex-initial">
+                <img src={(pinDetail?.image && urlFor(pinDetail?.image).url())} alt="user-post" className="rounded-t-3xl rounded-b-lg"/>
+            </div>
+            <div>
+                
+            </div>
         </div>
-    )
+    );
 };
 
 export default PinDetails;
