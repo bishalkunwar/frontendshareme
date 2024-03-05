9715caa6565b86683c5eb0563c4a3ea76b03caf7
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-05 16:38:41 -0500

    error solved connecting to sanity

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import { createClient } from "@sanity/client";
 import ImageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-    projectId: `${process.env.SHAREME_SANITY_PROJECT_ID}`,
+    projectId: "hptyd5o4",
     dataset: 'production',
     apiVersion: '2023-02-06',
     useCdn: true,
-    token: `${process.env.SHAREME_SANITY_PROJECT_TOKEN}`,
+    token: "skXEyjo46YTimRFKqAAHvZUBhWFJqjiO6EnuEysVB1iJDJX3cFA6KxOhGo6tBjOo8MQrObaqwTqtmjumybMKtwo6lBCZsaKmwELqcyZ4QWhBxBgIwXzY0ZljSqJs0lBt0hce4LM812XeYx7TPf5dzfUVtmJURiKfFcqj5qZY7kGBX3qdJOvK",
 });
 
 //console.log(process.env.SHAREME_SANITY_PROJECT_ID);
